24c09c5aefd4ac01738dd0b65b1f297358446730
chenhaiqian <1837610193@qq.com>
2019-09-22 15:42:48 +0800

    不同用户登录

--- src/main/java/com/zjsru/controller/UserController.java
+++ src/main/java/com/zjsru/controller/UserController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.zjsru.bean.Manager;
 import com.zjsru.bean.User;
+import com.zjsru.service.ManagerService;
 import com.zjsru.service.UserService;
 
 @Controller
@@ -21,6 +23,8 @@ import com.zjsru.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private ManagerService managerService;
 
 	@RequestMapping("/login")
 	public String login() {
@@ -38,12 +42,22 @@ public class UserController {
 		}
 		return "fail.jsp";
 	}
+	@RequestMapping(value = "/checkManagerLogin", method = RequestMethod.POST)
+	public String checkManagerLogin(HttpServletRequest req, Model model) throws UnsupportedEncodingException {
+		req.setCharacterEncoding("UTF-8");
+		Manager manager = new Manager();
+		managerService.checkLogin(manager.getId(),manager.getPassword());
+		if (manager != null) {
+			model.addAttribute("manager", manager);
+			return "redirect:/";
+		}
+		return "fail.jsp";
+	}
 
 	@RequestMapping("/regist")
 	public String regist() {
 		return "regist.jsp";
 	}
-
 	@RequestMapping(value = "/doRegist", method = RequestMethod.POST)
 	public String doRegist(HttpServletRequest req, Model model) throws UnsupportedEncodingException {
 		req.setCharacterEncoding("UTF-8");
@@ -52,6 +66,20 @@ public class UserController {
 		user.setUname(req.getParameter("uname"));
 		user.setPassword(req.getParameter("password"));
 		userService.Regist(user);
+		return "success.jsp";
+	}
+	
+	@RequestMapping("/managerRegist")
+	public String managerRegist() {
+		return "managerRegist.jsp";
+	}
+	@RequestMapping(value = "/doManagerRegist", method = RequestMethod.POST)
+	public String doManagerRegist(HttpServletRequest req, Model model) throws UnsupportedEncodingException {
+		req.setCharacterEncoding("UTF-8");
+		Manager manager = new Manager();
+		manager.setId(Integer.parseInt(req.getParameter("id")));
+		manager.setPassword(req.getParameter("password"));
+		managerService.Regist(manager);
 		return "success.jsp";
 	}
 	
